7dac1e2a7dbe37db8c098a26ce8263d717c19281
Roma Hana <81316393+Roman-Hana@users.noreply.github.com>
2023-12-27 11:42:00 +0200

    initial commit

---
+++ app/(pages)/about/page.js
@@ -0,0 +1,8 @@
+import styles from './styles/about.module.scss';
+
+const About = () => {
+    return (
+        <div>About</div>
+    );
+};
+export default About;
---
+++ app/(pages)/animals/page.js
@@ -0,0 +1,8 @@
+import styles from './styles/animals.module.scss';
+
+const Animals = () => {
+    return (
+        <div>Animals</div>
+    );
+};
+export default Animals;
