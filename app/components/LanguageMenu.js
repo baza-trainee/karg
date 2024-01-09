b3ede43e0c36e1f2464b80ccb24b5d0e39a254eb
Meralis <lucy.klim.30@gmail.com>
2024-01-09 13:27:55 +0200

    feat: add switch language panel

---
+++ app/components/LanguageMenu.js
@@ -0,0 +1,29 @@
+'use client'
+import { useState } from 'react';
+import styles from '../styles/LanguageMenu.module.scss'
+
+export const LanguageMenu = () => {
+    const languages = ['ukr', 'eng'];
+    const [selectedLanguage, setSelectedlanguage] = useState(localStorage.getItem('language') || 'ukr');
+    const [isOpen, setOpen] = useState(false);
+
+    const switchLanguage = (language) => {
+        setSelectedlanguage(language);
+        localStorage.setItem('language', language);
+        setOpen(false);
+    }
+
+    return (
+        <div className={styles.languageMenu} onMouseOver={() => setOpen(true)} onMouseLeave={() => setOpen(false)}>
+            <div className={styles.selectedLanguage}>{selectedLanguage}</div>
+            <ul className={`${styles.languageList} ${isOpen ? styles.active : ''}`} >
+                {languages
+                    .filter(language => language !== selectedLanguage)
+                    .map((language) => (
+                        <li className={styles.languageItem} key={language} onClick={() => switchLanguage(language)}>{language}</li>
+                    ))}
+            </ul>
+            <div><img src='/check-mark-icon.png' alt='check mark icon'></img></div>
+        </div>
+    );
+}
--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import HeroSection from "@/components/Hero/hero-section";
+import { LanguageMenu } from './components/LanguageMenu';
 import styles from "./styles/main.module.scss";
 
 export default function Home() {
   return (
     <div className={styles.main}>
       <HeroSection />
+      <LanguageMenu />
     </div>
   );
 }
