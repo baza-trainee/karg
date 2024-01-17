7f710d3f3e3fde387806e19522fbd2421159f7e4
Roma Hana <81316393+Roman-Hana@users.noreply.github.com>
2024-01-17 19:07:00 +0200

    feat: add header mobile and desktop versions

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import HeroSection from "@/components/Hero/hero-section";
 import styles from "./styles/main.module.scss";
 import MissionSection from "@/components/Mission/mission-section";
+import Header from "@/components/Header/header";
 
 export default function Home() {
   return (
     <div className={styles.main}>
+      <Header />
       <HeroSection />
       <MissionSection />
     </div>
