f9f887b290845108649ae3fae23f7dfafc068b41
Roma Hana <81316393+Roman-Hana@users.noreply.github.com>
2024-01-22 12:22:44 +0200

    bugfix: merging problem solving

--- app/[locale]/page.js
+++ app/[locale]/page.js
@@ -1,10 +1,10 @@
 import HeroSection from "@/components/Hero/hero-section";
 import QuickAccessMenu from "@/components/QuickAccessMenu/QuickAccessMenu";
+import LanguageMenu from '@/components/LanguageMenu/LanguageMenu';
 import initTranslations from "../i18n";
 import TranslationsProvider from "@/components/TranslationsProvider";
 import styles from "./styles/main.module.scss";
 import MissionSection from "@/components/Mission/mission-section";
-import Achievements from "@/components/Achievements/Achievements";
 import Header from "@/components/Header/header";
 
 const i18nNamespaces = ['home', 'common'];
@@ -16,10 +16,10 @@ export default async function Home({ params: { locale } }) {
     <TranslationsProvider resources={resources} locale={locale} namespaces={i18nNamespaces}>
       <div className={styles.main}>
         <Header />
+        <LanguageMenu />
         <HeroSection />
         <QuickAccessMenu />
         <MissionSection />
-        <Achievements/>
       </div>
     </TranslationsProvider>
   );
