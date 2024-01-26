f33c84c551b5d8a3f19d0f1d4c39f33b1107edc4
Julie Shapovalenko <jools.shapo@gmail.com>
2024-01-26 12:54:21 +0200

    refactor: implement image from next instead if bg-image

--- components/Hero/hero-section.js
+++ components/Hero/hero-section.js
@@ -1,4 +1,8 @@
 import styles from "./styles/hero.module.scss";
+import Image from "next/image";
+import heroImageMob from "@/public/assets/images/main/hero-mob.jpg";
+import heroImageTab from "@/public/assets/images/main/hero-tablet-cut.jpg";
+import heroImageDesk from "@/public/assets/images/main/hero-desktop-cut.jpg";
 
 const HeroSection = () => {
   return (
@@ -7,6 +11,15 @@ const HeroSection = () => {
         Kyiv animal <br />
         rescue group
       </h1>
+      <div className={styles.mobImage}>
+        <Image src={heroImageMob} alt="Picture of the team" sizes="100vw" />
+      </div>
+      <div className={styles.tabImage}>
+        <Image src={heroImageTab} alt="Picture of the team" sizes="100vw" />
+      </div>
+      <div className={styles.deskImage}>
+        <Image src={heroImageDesk} alt="Picture of the team" sizes="100vw" />
+      </div>
     </section>
   );
 };
