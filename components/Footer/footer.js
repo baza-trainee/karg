b558e744035e74819c1190f3c40058b5b50bb92c
Roma Hana <81316393+Roman-Hana@users.noreply.github.com>
2024-01-28 19:43:36 +0200

    feat: Add submenu functionality to the footer and desktop view

--- components/Footer/footer.js
+++ components/Footer/footer.js
@@ -6,6 +6,7 @@ import variables from "@/app/[locale]/variables.module.scss";
 import { Logo, ArrowDown } from "@/public/assets/icons";
 import useToggle from '@/utils/useToggle';
 import SocialIcons from '../SocialIcons/socialIcons';
+import ButtonAsLinkAsLink from '@/components/ButtonAsLink/buttonAsLink';
 
 
 
@@ -13,6 +14,12 @@ function Footer() {
 
     const [openFirst, setFirstOpen] = useToggle(false);
     const [openSecond, setSecondOpen] = useToggle(false);
+    const [openSubMenu, setSubMenuOpen] = useToggle(false);
+
+    const handleSubMenu = () => {
+        setSubMenuOpen(true);
+        setFirstOpen();
+    };
 
     return (
         <footer className={styles.footer}>
@@ -22,18 +29,24 @@ function Footer() {
             </div>
             <nav className={`${styles.navMenu} ${variables.button2}`}>
                 <ul>
-                    <li><a href="#">Головна</a></li>
-                    <li><p className={styles.navMenuInnerMenu} onClick={setFirstOpen}><span>Про нас</span><ArrowDown /></p>
-                        {openFirst ? <ul className={styles.navMenuInnerList}>
-                            <li><a href="#">Історія виникнення</a></li>
-                            <li><a href="#">Правила звернення</a></li>
-                            <li><a href="#">Наша команда</a></li>
-                            <li><a href="#">Контакти</a></li>
-                        </ul> : null}
+                    <li className={`${openSubMenu ? styles.navMenuLI : null}`}><a href="#">Головна</a></li>
+                    <li><p className={`${styles.navMenuInnerMenu}`} onClick={() => handleSubMenu()}><span>Про нас</span><ArrowDown /></p>
+                        {openFirst ? <>
+                            <ul className={styles.navMenuInnerList}>
+                                <li><a href="#">Історія виникнення</a></li>
+                                <li><a href="#">Правила звернення</a></li>
+                            </ul>
+                            <ul className={styles.navMenuInnerList}>
+                                <li><a href="#">Наша команда</a></li>
+                                <li><a href="#">Контакти</a></li>
+                            </ul>
+                        </> : null}
                     </li>
-                    <li><a href="#">Наші тварини</a></li>
-                    <li><a href="#">Допомогти нам</a></li>
-                    <li><p className={styles.navMenuInnerMenu} onClick={setSecondOpen}><span>Корисне</span><ArrowDown /></p>
+                    <li className={`${openSubMenu ? styles.navMenuLI : null}`}><a href="#">Наші тварини</a></li>
+                </ul>
+                <ul>
+                    <li className={`${openSubMenu ? styles.navMenuLI : null}`}><a href="#">Допомогти нам</a></li>
+                    <li className={`${openSubMenu ? styles.navMenuLI : null}`}><p className={styles.navMenuInnerMenu} onClick={setSecondOpen}><span>Корисне</span><ArrowDown /></p>
                         {openSecond ? <ul className={styles.navMenuInnerList}>
                             <li><a href="#">Поради</a></li>
                             <li><a href="#">FAQ</a></li>
@@ -42,11 +55,18 @@ function Footer() {
                     </li>
                 </ul>
             </nav>
+            <ButtonAsLinkAsLink route="/help" buttonCaption="Підтримати" buttonStyle="primary-lite-W-288" />
             <div className={`${styles.contactsContainer} ${variables.button2}`}>
-                <a href="tel: +380939862262">+38 (093) 986-2262</a>
-                <a href="tel: +380988447937">+38 (098) 844-7937</a>
-                <a href="mailto: karg.inform@gmail.com">karg.inform@gmail.com</a>
-                <a href="https://maps.app.goo.gl/4Ra4rk12B7hkwKmM6" target="_blank">м. Київ</a>
+                <div className={styles.contactsContainerTelephones}>
+                    <a href="tel: +380939862262">+38 (093) 986-2262</a>
+                    <a href="tel: +380988447937">+38 (098) 844-7937</a>
+                </div>
+                <div className={styles.contactsContainerEmail}>
+                    <a href="mailto: karg.inform@gmail.com">karg.inform@gmail.com</a>
+                </div>
+                <div className={styles.contactsContainerLocation}>
+                    <a href="https://maps.app.goo.gl/4Ra4rk12B7hkwKmM6" target="_blank">м. Київ</a>
+                </div>
             </div>
             <SocialIcons iconsColor='#A1A1A1' />
             <p className={styles.signature}>Розробка Baza Trainee Ukraine © 2023 Всі права захищені</p>
