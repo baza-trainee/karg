3c725a1f44e615bd343c940caa368d33a264ad5d
Julie Shapovalenko <jools.shapo@gmail.com>
2024-02-02 17:13:57 +0200

    feat: update component and styles of history

--- app/[locale]/(pages)/about/history_of_origin/page.js
+++ app/[locale]/(pages)/about/history_of_origin/page.js
@@ -1,4 +1,5 @@
 import styles from "./historyOfOrigin.module.scss";
+import btnLinkStyles from "@/components/ButtonAsLink/buttonAsLink.module.scss";
 
 import initTranslations from "@/app/i18n";
 import TranslationsProvider from "@/components/TranslationsProvider";
@@ -34,9 +35,9 @@ const historyOfOrigin = async ({ params: { locale } }) => {
     >
       <div className={styles.history}>
         <Header />
-        <div>
-          <h3>Історія виникнення команди</h3>
-          <p>
+        <div className={styles.container}>
+          <h3 className={styles.historyTitle}>Історія виникнення команди</h3>
+          <p className={styles.historyText}>
             Жодна державна чи комунальна установа в Україні не спеціалізується
             на рятуванні диких, безхатніх чи домашніх тварин, що потрапили в
             смертельно небезпечне становище. Тому ці завдання лягають на плечі
@@ -65,6 +66,25 @@ const historyOfOrigin = async ({ params: { locale } }) => {
                 height: "127px",
               }}
             />
+            <Image
+              className={styles.historyTab}
+              src={historyTabOne}
+              alt="Team seat on the car"
+              style={{
+                width: "706px",
+                height: "311px",
+              }}
+            />
+            <Image
+              className={styles.historyDesk}
+              src={historyDeskOne}
+              alt="Team seat on the car"
+              style={{
+                width: "1170px",
+                height: "361px",
+              }}
+            />
+
             <div className={styles.imgInnerWrap}>
               <Image
                 className={styles.historyMob}
@@ -76,6 +96,25 @@ const historyOfOrigin = async ({ params: { locale } }) => {
                 }}
               />
               <Image
+                className={styles.historyTab}
+                src={historyTabTwo}
+                alt="Girl in the boat"
+                style={{
+                  width: "343px",
+                  height: "429px",
+                }}
+              />
+              <Image
+                className={styles.historyDesk}
+                src={historyDeskTwo}
+                alt="Girl in the boat"
+                style={{
+                  width: "570px",
+                  height: "529px",
+                }}
+              />
+
+              <Image
                 className={styles.historyMob}
                 src={historyMobThree}
                 alt="Boy with a swan"
@@ -84,10 +123,75 @@ const historyOfOrigin = async ({ params: { locale } }) => {
                   height: "170px",
                 }}
               />
+              <Image
+                className={styles.historyTab}
+                src={historyTabThree}
+                alt="Boy with a swan"
+                style={{
+                  width: "343px",
+                  height: "429px",
+                }}
+              />
+              <Image
+                className={styles.historyDesk}
+                src={historyDeskThree}
+                alt="Boy with a swan"
+                style={{
+                  width: "570px",
+                  height: "529px",
+                }}
+              />
             </div>
           </div>
+          <p className={styles.historyText}>
+            Саме так працює KARG — структура, що починалася з особистої
+            ініціативи двох волонтерів — Михайла Сторожука та Любови Кравчук — і
+            перетворилася на громадську організацію, яка опікується порятунком
+            тварин. У її розпорядженні є чотири рятувальні автомобілі та один
+            пожежний, а сама організація нараховує понад двадцять людей. Щодня
+            на гарячу лінію Команди порятунку надходять дзвінки: зазвичай
+            звертаються із запитами кияни, однак телефонують також люди з
+            передмістя. А часом навіть із інших регіонів країни. Бо сьогодні
+            вкрай важко знайти організацію, члени якої не лише володіли б
+            специфічними навичками порятунку тварин, а й погодилися робити це за
+            невелику плату, а подеколи й безкоштовно.
+          </p>
+          <div>
+            <Image
+              className={styles.historyMob}
+              src={historyMobFour}
+              alt="Team seat on the car"
+              style={{
+                width: "288px",
+                height: "127px",
+              }}
+            />
+            <Image
+              className={styles.historyTab}
+              src={historyTabFour}
+              alt="Team seat on the car"
+              style={{
+                width: "706px",
+                height: "311px",
+              }}
+            />
+            <Image
+              className={styles.historyDesk}
+              src={historyDeskFour}
+              alt="Team seat on the car"
+              style={{
+                width: "1170px",
+                height: "455px",
+              }}
+            />
+          </div>
+          <ButtonAsLink
+            route="/our_team"
+            buttonCaption="Про команду"
+            buttonStyle={`${btnLinkStyles.rescue}`}
+          />
         </div>
-        <ButtonAsLink />
+
         <Footer />
       </div>
     </TranslationsProvider>
