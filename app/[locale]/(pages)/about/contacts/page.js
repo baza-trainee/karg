8d99ef8106127c460f73f034f0cec731eb61ee1b
Julie Shapovalenko <jools.shapo@gmail.com>
2024-02-07 16:12:02 +0200

    feat: create contacts page, add images

--- app/[locale]/(pages)/about/contacts/page.js
+++ app/[locale]/(pages)/about/contacts/page.js
@@ -1,9 +1,48 @@
-import React from 'react';
+import styles from "./contacts.module.scss";
 
-const Contacts = () => {
-    return (
-        <div>Contacts</div>
-    );
+import initTranslations from "@/app/i18n";
+import TranslationsProvider from "@/components/TranslationsProvider";
+
+import Header from "@/components/Header/header";
+import Footer from "@/components/Footer/footer";
+import PageHero from "@/components/common/PageHero/pageHero";
+
+import Image from "next/image";
+import {
+  contactsHeroMob,
+  contactsHeroTab,
+  contactsHeroDesk,
+  contactsMobOne,
+  contactsTabOne,
+  contactsDeskOne,
+} from "@/public/assets/images/about/contacts/index";
+
+const i18nNamespaces = ["home", "common"];
+
+const Contacts = async ({ params: { locale } }) => {
+  const { t, resources } = await initTranslations(locale, i18nNamespaces);
+
+  const altText = "Squirrel sit on a tree";
+  const buttonText = "Контакти";
+
+  return (
+    <TranslationsProvider
+      resources={resources}
+      locale={locale}
+      namespaces={i18nNamespaces}
+    >
+      <Header />
+      <PageHero
+        mobImage={contactsHeroMob.src}
+        tablImage={contactsHeroTab.src}
+        deskImage={contactsHeroDesk.src}
+        buttonText={buttonText}
+        altText={altText}
+      />
+      Contacts
+      <Footer />
+    </TranslationsProvider>
+  );
 };
 
 export default Contacts;
