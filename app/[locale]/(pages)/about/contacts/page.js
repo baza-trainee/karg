1484a3c725af2e2bf7292c77941e957280384437
Julie Shapovalenko <jools.shapo@gmail.com>
2024-02-09 17:37:18 +0200

    feat: add styles

--- app/[locale]/(pages)/about/contacts/page.js
+++ app/[locale]/(pages)/about/contacts/page.js
@@ -6,6 +6,7 @@ import TranslationsProvider from "@/components/TranslationsProvider";
 import Header from "@/components/Header/header";
 import Footer from "@/components/Footer/footer";
 import PageHero from "@/components/common/PageHero/pageHero";
+import SocialIcons from "@/components/SocialIcons/socialIcons";
 
 import Image from "next/image";
 import {
@@ -22,26 +23,27 @@ import {
   EnvelopeIconContacts,
   ClockIconContacts,
 } from "@/public/assets/icons/index";
+import Link from "next/link";
 
 const i18nNamespaces = ["home", "common"];
 
 const contactsData = [
   {
     id: 1,
-    icon: <PhoneIconContacts />,
+    icon: <PhoneIconContacts width={21} height={21} />,
     title: "Контактний телефон",
     data: ["+38 (093) 986-2262", "+38 (098) 844-7937"],
-    href: ["tell:+380939862262", "tell:+380988447937"],
+    href: ["tel:+380939862262", "tel:+380988447937"],
   },
   {
     id: 2,
-    icon: <ClockIconContacts />,
+    icon: <ClockIconContacts width={22} height={22} />,
     title: "Графік роботи",
     data: "Екстренні виклики (із загрозою для життя тварини) приймаються цілодобово",
   },
   {
     id: 3,
-    icon: <EnvelopeIconContacts />,
+    icon: <EnvelopeIconContacts width={21} height={17} />,
     title: "Електронна пошта",
     data: "karg.inform@gmail.com",
     href: "mailto:karg.inform@gmail.com",
@@ -50,21 +52,27 @@ const contactsData = [
 
 const ContactItem = ({ id, icon, title, data, href }) => {
   return (
-    <div>
-      {icon}
-      <h4>{title}</h4>
-      {id === 1 && (
-        <ul>
-          <li>
-            <a href={href[0]}>{data[0]}</a>
-          </li>
-          <li>
-            <a href={href[1]}>{data[1]}</a>
-          </li>
-        </ul>
-      )}
-      {id === 2 && <p>{data}</p>}
-      {id === 3 && <a href={href}>{data}</a>}
+    <div className={styles.contact}>
+      <span className={styles.contactIcon}>{icon}</span>
+      <div className={styles.contactWrap}>
+        <h4 className={styles.contactTitle}>{title}</h4>
+        {id === 1 && (
+          <ul className={styles.contactList}>
+            <li className={styles.contactItem}>
+              <a href={href[0]}>{data[0]}</a>
+            </li>
+            <li className={styles.contactItem}>
+              <a href={href[1]}>{data[1]}</a>
+            </li>
+          </ul>
+        )}
+        {id === 2 && <p className={styles.contactItem}>{data}</p>}
+        {id === 3 && (
+          <a className={styles.contactItem} href={href}>
+            {data}
+          </a>
+        )}
+      </div>
     </div>
   );
 };
@@ -82,31 +90,76 @@ const Contacts = async ({ params: { locale } }) => {
       namespaces={i18nNamespaces}
     >
       <Header />
-      <PageHero
-        mobImage={contactsHeroMob.src}
-        tablImage={contactsHeroTab.src}
-        deskImage={contactsHeroDesk.src}
-        buttonText={buttonText}
-        altText={altText}
-      />
-      <h3>Як нас знайти</h3>
-      <div>
-        <ul>
-          {contactsData?.map((c) => {
-            return (
-              <li key={c?.id}>
-                <ContactItem
-                  id={c?.id}
-                  icon={c?.icon}
-                  title={c?.title}
-                  data={c?.data}
-                  href={c?.href}
+      <main>
+        <PageHero
+          mobImage={contactsHeroMob.src}
+          tablImage={contactsHeroTab.src}
+          deskImage={contactsHeroDesk.src}
+          buttonText={buttonText}
+          altText={altText}
+        />
+        <section className={styles.contacts}>
+          <h3 className={styles.contactsTitle}>Як нас знайти</h3>
+          <div className={styles.contactsWrap}>
+            <ul className={styles.contactsList}>
+              {contactsData?.map((c) => {
+                return (
+                  <li className={styles.contactsItem} key={c?.id}>
+                    <ContactItem
+                      id={c?.id}
+                      icon={c?.icon}
+                      title={c?.title}
+                      data={c?.data}
+                      href={c?.href}
+                    />
+                  </li>
+                );
+              })}
+            </ul>
+            <div className={styles.socImgWrap}>
+              <Image
+                className={styles.imgMob}
+                src={contactsMobOne}
+                alt="A man is standing backside in a jacket with a signature Animal rescue team"
+                style={{
+                  width: "100vw",
+                  height: "100%",
+                }}
+              />
+              <Image
+                className={styles.imgTab}
+                src={contactsTabOne}
+                alt="A man is standing backside in a jacket with a signature Animal rescue team"
+                style={{
+                  width: "100%",
+                  height: "100%",
+                }}
+              />
+              <Image
+                className={styles.imgDesk}
+                src={contactsDeskOne}
+                alt="A man is standing backside in a jacket with a signature Animal rescue team"
+                style={{
+                  width: "100vw",
+                  height: "100%",
+                }}
+              />
+              <div className={styles.contactsItem}>
+                <h4 className={styles.contactTitle}>Соціальні мережі</h4>
+                <SocialIcons
+                  className={styles.iconSizes}
+                  iconsColor="#6B5199"
+                  gridOption={{
+                    width: "100%",
+                    justifyContent: "space-around",
+                  }}
+                  
                 />
-              </li>
-            );
-          })}
-        </ul>
-      </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
       <Footer />
     </TranslationsProvider>
   );
