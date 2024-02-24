37ea19dafe0c4bbd0fce06ab4a4edecb28b22521
Anastasiya <nastyadorul@gmail.com>
2024-02-24 14:31:43 +0200

    changes_in_state

--- app/[locale]/(pages)/login/page.js
+++ app/[locale]/(pages)/login/page.js
@@ -1,6 +1,6 @@
 import React from 'react';
-// import Login from '../../../../components/Login/Login';
-import Login from '../../../../components/adminPanel/Login/Login';
+import Login from '../../../../components/Login/Login';
+//import Login from '../../../../components/adminPanel/Login/Login';
 
 export default function LoginPage() {
   return (
