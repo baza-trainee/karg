d1cd76e2595f3cd098a5d2981ce721af445e9c44
Julie Shapovalenko <jools.shapo@gmail.com>
2024-02-28 13:22:50 +0200

    feat: add page structure

--- app/[locale]/(pages)/useful/faq/page.js
+++ app/[locale]/(pages)/useful/faq/page.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import styles from "./faq.module.scss";
 
-const Faq = () => {
-    return (
-        <div>Faq</div>
-    );
+import initTranslations from "@/app/i18n";
+import TranslationsProvider from "@/components/TranslationsProvider";
+
+import Header from "@/components/Header/header";
+import Footer from "@/components/Footer/footer";
+import PageHero from "@/components/common/PageHero/pageHero";
+
+import Image from "next/image";
+
+const i18nNamespaces = ["home", "common"];
+
+const Faq = async ({ params: { locale } }) => {
+  const { t, resources } = await initTranslations(locale, i18nNamespaces);
+
+  return (
+    <TranslationsProvider
+      resources={resources}
+      locale={locale}
+      namespaces={i18nNamespaces}
+    >
+      <Header />
+      <main>
+        <PageHero
+        //   mobImage={contactsHeroMob.src}
+        //   tablImage={contactsHeroTab.src}
+        //   deskImage={contactsHeroDesk.src}
+        //   buttonText={buttonText}
+        //   altText={altText}
+        />
+        Faq
+      </main>
+      <Footer />
+    </TranslationsProvider>
+  );
 };
 
 export default Faq;
