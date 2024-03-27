5e1fa8685aec30efa7705146efcdf6bb05f373aa
Julie Shapovalenko <jools.shapo@gmail.com>
2024-03-27 16:50:53 +0200

    fix: bug-202.15.1

--- app/[locale]/(pages)/about/history_of_origin/page.js
+++ app/[locale]/(pages)/about/history_of_origin/page.js
@@ -29,7 +29,6 @@ import NeedInfo from "@/components/common/NeedInfo/needInfo";
 
 const i18nNamespaces = ["historyOfOrigin", "common"];
 
-
 const historyOfOrigin = async ({ params: { locale } }) => {
   const { t, resources } = await initTranslations(locale, i18nNamespaces);
 
@@ -48,14 +47,14 @@ const historyOfOrigin = async ({ params: { locale } }) => {
           mobImage={historyHeroMob.src}
           tablImage={historyHeroTab.src}
           deskImage={historyHeroDesk.src}
-          buttonText={t('pageHeroButtonText')}
+          buttonText={t("pageHeroButtonText")}
           altText={altText}
         />
         <div className={styles.container}>
-          <h3 className={styles.historyTitle}>{t('historyOfOriginTitle')}</h3>
+          <h3 className={styles.historyTitle}>{t("historyOfOriginTitle")}</h3>
           <div className={`${styles.historyText} ${styles.historyTextTop}`}>
-            <p>{t('historyOfOriginTextFirstPart')}</p>
-            <p>{t('historyOfOriginTextSecondPart')}</p>
+            <p>{t("historyOfOriginTextFirstPart")}</p>
+            <p>{t("historyOfOriginTextSecondPart")}</p>
           </div>
           <div className={`${styles.imgOuterWrap} ${styles.imgOuterWrapTop}`}>
             <Image
@@ -163,7 +162,7 @@ const historyOfOrigin = async ({ params: { locale } }) => {
             </div>
           </div>
           <p className={`${styles.historyText} ${styles.historyTextBottom}`}>
-          {t('historyOfOriginTextThirdPart')}
+            {t("historyOfOriginTextThirdPart")}
           </p>
           <div
             className={`${styles.imgOuterWrap} ${styles.imgOuterWrapBottom}`}
@@ -205,15 +204,15 @@ const historyOfOrigin = async ({ params: { locale } }) => {
           <div className={styles.btnWrap}>
             <ButtonAsLink
               route="/about/our_team"
-              buttonCaption={t('historyOfOriginButtonText')}
+              buttonCaption={t("historyOfOriginButtonText")}
               buttonStyle="rescue"
             />
           </div>
         </div>
         <NeedInfo
-          title={t('needInfoTitle')}
-          subtitle={t('needInfoSubtitle')}
-          buttonCaption={t('needInfoButtonCaption')}
+          title={t("needInfoTitle")}
+          subtitle={t("needInfoSubtitle")}
+          buttonCaption={t("needInfoButtonCaption")}
           route={route}
         />
         <Footer />
