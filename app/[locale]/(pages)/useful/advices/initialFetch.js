c3dc25997779ee561026314f3948db896c31e407
Roma Hana <81316393+Roman-Hana@users.noreply.github.com>
2025-01-29 19:22:31 +0200

    add fetch-initial-cards component

--- app/[locale]/(pages)/useful/advices/initialFetch.js
+++ app/[locale]/(pages)/useful/advices/initialFetch.js
@@ -1,23 +1,10 @@
-import axios from 'axios';
+import fetchInitialCards from '@/components/FetchInitialCards/FetchInitialCards';
 import MultiPageCardItem from '@/components/MultiPageCardItem/multiPageCardItem';
 
-async function fetchInitialCards(locale) {
-    const currentPage = 1;
-    const cultureCode = locale === 'uk' ? 'ua' : 'en';
-    // console.log('Current locale:', locale);
 
-    try {
-        const response = await axios.get(`https://karg-backend-rkb4.onrender.com/karg/advice/getall?page=${currentPage}&pageSize=6&cultureCode=${cultureCode}`);
-
-        return response.data.advices || [];
-    } catch (error) {
-        console.error('Error fetching initial cards:', error);
-        return [];
-    }
-}
 
 export default async function InitialFetch({ locale }) {
-    const initialCards = await fetchInitialCards(locale);
+    const initialCards = await fetchInitialCards(locale, 'advice', 'getall');
 
     return (
         <>
---
+++ components/FetchInitialCards/FetchInitialCards.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+export default async function fetchInitialCards(locale, endpoint, method) {
+    const BASE_URL = 'https://karg-backend.onrender.com/karg';
+    const currentPage = 1;
+    const cultureCode = locale === 'uk' ? 'ua' : 'en';
+
+    try {
+        const response = await axios.get(`${BASE_URL}/${endpoint}/${method}?page=${currentPage}&pageSize=6&cultureCode=${cultureCode}`);
+
+        switch (endpoint) {
+            case "advice":
+                return response.data.advices || [];
+
+            case "animal":
+                return response.data.animals || [];
+        }
+
+    } catch (error) {
+        console.error('Error fetching initial cards:', error);
+        return [];
+    }
+}
