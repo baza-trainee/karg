262871c83d81b622ebf8cc41bfcc0562b02134f9
Roma Hana <81316393+Roman-Hana@users.noreply.github.com>
2025-02-24 20:12:59 +0200

    fix: замена функции получения FAQ на axios запрос в компоненте Faq

--- app/[locale]/(pages)/useful/faq/page.js
+++ app/[locale]/(pages)/useful/faq/page.js
@@ -12,8 +12,9 @@ import {
 } from "@/public/assets/images/useful/faq";
 import { FaqItem } from "@/components/FaqItem/faq-item";
 import ScrollToTop from "@/components/common/ScrollToTop/scrollToTop";
-import { getAllFAQ } from "@/app/[locale]/(pages)/dashboard/FAQ/api";
+// import { getAllFAQ } from "@/app/[locale]/(pages)/dashboard/FAQ/api";
 import styles from "./faq.module.scss";
+import axios from 'axios';
 
 const buttonText = "FAQ";
 const altText = "bats sit on a branch";
@@ -24,6 +25,14 @@ const Faq = async ({ params: { locale } }) => {
 
   let cultureCode = (locale === "uk") ? "ua" : "en";
   // const response = await getAllFAQ("", cultureCode);
+  let API_URL = `https://dev.karg.kyiv.ua/api/faq/getall?page=1&pageSize=6&cultureCode=${cultureCode}`;
+
+  let response = null;
+  try {
+    response = await axios.get(API_URL);
+  } catch (error) {
+    console.error("Error fetching FAQ:", error);
+  }
 
   return (
     <TranslationsProvider
@@ -41,13 +50,17 @@ const Faq = async ({ params: { locale } }) => {
           altText={altText}
           priority={true}
         />
-        {/* <ul className={styles.questionsList}>
-          {response?.map(({ id, question, answer }) => (
-            <li key={id}>
-              <FaqItem q={question} a={answer} />
-            </li>
-          ))}
-        </ul> */}
+        <ul className={styles.questionsList}>
+          {response && response.data && response.data.items && Array.isArray(response.data.items) ? (
+            response.data.items.map(({ id, question, answer }) => (
+              <li key={id}>
+                <FaqItem q={question} a={answer} />
+              </li>
+            ))
+          ) : (
+            <li>No FAQs found</li>
+          )}
+        </ul>
       </main>
       <ScrollToTop />
       <Footer />
