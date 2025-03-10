7c756dc19f2a6dc266db65a8338266a5c45e5633
Meralis <lucy.klim.30@gmail.com>
2025-03-10 17:35:09 +0200

    fix: fix styles

--- app/[locale]/(pages)/dashboard/Aside/LogoBlock.js
+++ app/[locale]/(pages)/dashboard/Aside/LogoBlock.js
@@ -4,18 +4,22 @@ import React from 'react';
 import AsideItem from './AsideItem';
 import { LogoIcon } from '@/public/assets/icons/aside';
 import styles from "./styles/aside.module.scss";
+import Link from "next/link";
+
 
 export default function LogoBlock() {
   return (
     <div className={styles.logo_name}>
-      <AsideItem
-        itemStyle={styles.item_logo}
-        titleStyle={styles.logo_title}
-        wrapperStyle={styles.wrapper_logo}
-        title="Kyiv animal rescue group"
-      >
-        <LogoIcon className={styles.logo} />
-      </AsideItem>
+      <Link href="/">
+        <AsideItem
+          itemStyle={styles.item_logo}
+          titleStyle={styles.logo_title}
+          wrapperStyle={styles.wrapper_logo}
+          title="Kyiv animal rescue group"
+        >
+          <LogoIcon className={styles.logo} />
+        </AsideItem>
+      </Link>
     </div>
   )
 }
