4fd6447d97d6721a187d5d167282bbaa278c3d98
Roma Hana <81316393+Roman-Hana@users.noreply.github.com>
2025-04-07 19:36:04 +0300

    feat: add next-sitemap configuration and integrate into build process

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    siteUrl: process.env.NEXT_PUBLIC_API_BASE_URL || "https://dev.karg.kyiv.ua",
+    generateRobotsTxt: true,
+    changefreq: "monthly",
+    priority: 0.7,
+    sitemapSize: 5000,
+    exclude: ["/auth/login", "/dashboard"],
+};
