341edeaa7740bf2392dfa5bab664eda894c1303c
Meralis <lucy.klim.30@gmail.com>
2025-04-22 14:40:59 +0300

    fix: misc-fixes-and-improvements

--- app/[locale]/(pages)/dashboard/ProtectedRoute.js
+++ app/[locale]/(pages)/dashboard/ProtectedRoute.js
@@ -3,11 +3,14 @@ import { useContext, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { AdminContext } from '@/app/adminProvider';
 import { useState } from 'react';
+import  ModalContext  from '@/app/ModalContext';
+import handleForbiddenAccess from './handleForbiddenAccess';
 
 const ProtectedRoute = ({ children }) => {
-    const { accountId } = useContext(AdminContext);
+    const { accountId, logoutDependencies } = useContext(AdminContext);
     const router = useRouter();
     const [isMounted, setIsMounted] = useState(false);
+    const { showModal } = useContext(ModalContext);
 
     useEffect(() => {
         if (typeof window !== 'undefined') {
@@ -26,6 +29,18 @@ const ProtectedRoute = ({ children }) => {
             router.push('/auth/login');
         }
     }, [isMounted, router]);
+
+    useEffect(() => {
+        const handleStorageListener = (e) => {
+            if (e.key === 'auth-token' && e.newValue === null) {
+                handleForbiddenAccess(router, showModal, logoutDependencies);
+            }
+        }
+        window.addEventListener('storage', handleStorageListener);
+        return () => {
+            window.removeEventListener('storage', handleStorageListener);
+        }
+    }, [])
 
     if (!isMounted) return <div style={{ display: 'none' }}></div>;
     return accountId ? children : null;
